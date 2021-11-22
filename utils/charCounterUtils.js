95fbb7692d046acb3b1b63e8138e33ff31a3c861
JFQ_DEV <jfqdev@gmail.com>
2021-11-22 11:13:05 -0300

    fixed

--- utils/charCounterUtils.js
+++ utils/charCounterUtils.js
@@ -1,11 +1,19 @@
 function charTotalCount( resourceData, param, char ){
     let totalCount = 0
-    resourceData.map(resource =>{
-      resource.data.results.map(result=>{
+    if(resourceData.length > 1){
+      resourceData.map(resource =>{
+        resource.data.results.map(result=>{
+          let paramData = result[`${param}`].toLowerCase()
+          totalCount += charCounter(paramData, char)
+        })
+      })
+    }else{ //In case there is only one page of records
+      resourceData.data.results.map(result=>{
         let paramData = result[`${param}`].toLowerCase()
         totalCount += charCounter(paramData, char)
       })
-    })
+    }
+
     return totalCount
 }
 
