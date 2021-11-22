24e0c992b1f4475765a5faa67054ea5e045ce65c
JFQ_DEV <jfqdev@gmail.com>
2021-11-22 00:29:35 -0300

    add charCounter and charTotalCount

---
+++ utils/charCounterUtils.js
@@ -0,0 +1,26 @@
+function charTotalCount( resourceData, param, char ){
+    let totalCount = 0
+    resourceData.map(resource =>{
+      resource.data.results.map(result=>{
+        let paramData = result[`${param}`].toLowerCase()
+        totalCount += charCounter(paramData, char)
+      })
+    })
+    return totalCount
+}
+
+function charCounter( paramData, char){
+let count = 0;
+let fromIndex = paramData.indexOf(char);
+
+while(fromIndex !== -1 ){
+    count += 1
+    fromIndex = paramData.indexOf(char, ++fromIndex);
+}
+return count
+}
+
+module.exports= {
+    charTotalCount,
+    charCounter
+}
