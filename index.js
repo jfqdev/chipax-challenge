f3b1783fdedec64191430299a41cf12c3373c192
JFQ_DEV <jfqdev@gmail.com>
2021-11-21 23:58:55 -0300

    solution first structure

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hello World')
+const fs = require('fs');
+
+async function solveChallenge({resources,selectedChallenge,charParams})
+{
+
+  switch (selectedChallenge) {
+    case "both":
+      //solution = [solveCharCounter(charParams),solveEpisodeLocations()]
+      break;
+    case "charCounter":
+      //solution = [solveCharCounter(charParams)]
+      break;
+    case "episodeLocations":
+      //solution = [solveEpisodeLocations()]
+      break;
+  }
+
+  //challengeJSON = JSON.stringify(solution);
+  //fs.writeFileSync('challengeJSON.json', challengeJSON);
+}
+
+solveChallenge({
+  resources: ['character','episode','location'], //Change only if Rick&Morty Api does.
+  selectedChallenge: 'both', // "both", "charCounter", "episodeLocations",
+  charParams:{letter:['c', 'e', 'l'], property:"name"} // charCounter params.
+})
